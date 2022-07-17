d6c394de710266ad8e38f2fd0beebbb27f880c42
Rémy Machavoine <remygt@hotmail.fr>
2022-07-17 15:58:30 +0200

    feat(webrtc): add webrtc

--- index.ts
+++ index.ts
@@ -3,9 +3,22 @@ import { getIO, initIO } from './server';
 
 const app = express();
 
+app.use(express.json());
+
+app.use(function (req, res, next) {
+    console.log("on passe dans le middleware");
+    
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', ['Content-Type','Authorization', '*']);
+
+    next();
+});
+
 const httpServer = require('http').createServer(app);
 
-let port = process.env.PORT || 3500;
+let port = process.env.PORT || 5000;
 
 initIO(httpServer);
 
--- server.ts
+++ server.ts
@@ -3,11 +3,18 @@ import { Server } from 'socket.io';
 let IO: any;
 
 export const initIO = (httpServer: Server) => {
-    IO = require('socket.io')(httpServer);
+    IO = require('socket.io')(httpServer, {
+        cors: {
+          origin: "*",
+          methods: ["GET", "POST"]
+        }
+      });
 
     IO.use((socket: any, next: any) => {
         if (socket.handshake.query) {
-            let userName = socket.handshake.query.name
+            console.log("middleware");
+            console.log(socket.handshake.query);
+            let userName = socket.handshake.query.user
             socket.user = userName;
             next();
         }
@@ -18,9 +25,10 @@ export const initIO = (httpServer: Server) => {
         socket.join(socket.user);
 
         socket.on('call', (data: any) => {
+            console.log("call");
+            console.log(data.name);
             let callee = data.name;
             let rtcMessage = data.rtcMessage;
-
             socket.to(callee).emit("newCall", {
                 caller: socket.user,
                 rtcMessage: rtcMessage
@@ -29,6 +37,8 @@ export const initIO = (httpServer: Server) => {
         })
 
         socket.on('answerCall', (data: any) => {
+            console.log("answer call");
+            console.log(data);
             let caller = data.caller;
             const rtcMessage = data.rtcMessage
 
@@ -40,6 +50,8 @@ export const initIO = (httpServer: Server) => {
         })
 
         socket.on('ICEcandidate', (data: any) => {
+            console.log("ice candidate call");
+            console.log(data);
             let otherUser = data.user;
             let rtcMessage = data.rtcMessage;
 
